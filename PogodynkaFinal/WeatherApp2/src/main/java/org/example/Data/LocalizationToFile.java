6585b5b47055913a1bab85034202c1a852d21af0
GGcollector <136164771+GGcollector@users.noreply.github.com>
2023-12-07 14:21:00 +0100

    Add files via upload

---
+++ PogodynkaFinal/WeatherApp2/src/main/java/org/example/Data/LocalizationToFile.java
@@ -0,0 +1,8 @@
+package org.example.Data;
+
+public interface LocalizationToFile {
+    static void saveCityDataToFile(String file, boolean shouldAppend, String... arguments) {
+    }
+    static void loadCityDataFromFile(String file) {
+    }
+}
